3a0fb73ad7e6b1456645b6902492cd27e9bb58e0
Krishnanunni <krishnanunnir97@gmail.com>
2019-03-13 23:36:48 +0530

    Added search route

--- search.py
+++ search.py
@@ -1,7 +1,12 @@
 from flask import Flask, redirect, url_for, request, render_template
 from urllib.request import urlopen
 import json
+import key
 app = Flask(__name__)
+
+@app.route('/search',methods = ['POST', 'GET'])
+def search():
+    return "This is the search page"
 
 if __name__ == '__main__':
    app.run(debug=True)
